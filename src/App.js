39ecd2a3f048bf142b33aef69feb56edfe236619
franjmr0416 <franjmr16@gmail.com>
2022-09-26 23:46:12 -0500

    formulario optimizado

--- src/App.js
+++ src/App.js
@@ -1,62 +1,30 @@
 import { useState } from "react";
-import Button from "./components/Button";
+
 import Card from "./components/Card";
 import Container from "./components/Container";
-import Input from "./components/Input";
-import useFormulario from "./hooks/useFormulario";
+import UserForm from "./components/UserForm";
 
 const App = () => {
   const [usuarios, setUsuarios] = useState([]);
-  const [formulario, handleChange, reset] = useFormulario({
-    name: "",
-    lastname: "",
-    email: "",
-  });
 
-  const submit = (e) => {
-    e.preventDefault();
-    setUsuarios([...usuarios, formulario]);
-    reset();
+  const submit = (usuario) => {
+    setUsuarios([...usuarios, usuario]);
   };
 
-  console.log(formulario, usuarios);
+  console.log(usuarios);
+
   return (
     <div style={{ marginTop: "15%" }}>
       <Container>
         <Card>
           <div style={{ padding: 20 }}>
-            <form onSubmit={submit}>
-              <Input
-                label="Nombre"
-                name="name"
-                value={formulario.name}
-                onChange={handleChange}
-                placeholder="Nombre"
-              />
-              <Input
-                label="Apellido"
-                name="lastname"
-                value={formulario.lastname}
-                onChange={handleChange}
-                placeholder="Apellido"
-              />
-              <Input
-                label="Correo"
-                name="email"
-                value={formulario.email}
-                onChange={handleChange}
-                placeholder="Correo"
-              />
-              <Button>Enviar</Button>
-            </form>
+            <UserForm submit={submit} />
           </div>
         </Card>
         <Card>
           <ul>
-            {usuarios.map((usuario) => (
-              <li
-                key={usuario.email}
-              >{`${usuario.name} ${usuario.lastname}: ${usuario.email}`}</li>
+            {usuarios.map((x) => (
+              <li key={x.email}>{`${x.name} ${x.lastname}: ${x.email}`}</li>
             ))}
           </ul>
         </Card>
